7c08ecccd4bbb6199b6cd18cbd150b7425390893
Yuliia <krekninajulia@mail.ru>
2023-03-26 18:57:59 +0300

    Tests

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,33 +1,57 @@
 package com.example.sweater;
 
-import org.json.simple.JSONObject;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
                            Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
 
-   /* @GetMapping("/")
-    public String main(Map <String, Object> model) {
-        model.put("some", "hello, lets code");
-        return "main";
-    }*/
-   @GetMapping("/")
-   @ResponseBody
-    public JSONObject main() {
-       JSONObject json = new JSONObject();
-       json.put("name", "Iuliia");
-       json.put("lastName", "Azarina");
-        return json;
+    /* @GetMapping("/")
+     public String main(Map <String, Object> model) {
+         model.put("some", "hello, lets code");
+         return "main";
+     }*/
+    @GetMapping("/")
+    @ResponseBody
+    public Cat main() {
+        Cat cat = new Cat("basik", 3);
+        return cat;
+    }
+}
+
+class Cat {
+    String name;
+    int age;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Cat() {
     }
 }
---
+++ src/test/java/com/example/sweater/LoginTest.java
@@ -0,0 +1,43 @@
+package com.example.sweater;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Приветствие")
+    public void greeting() throws Exception{
+        this.mockMvc.perform(get("/greeting"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("greeting")));
+    }
+    @Test
+    @DisplayName("Json Кота")
+    public void jsonCat() throws Exception{
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("{\"name\":\"basik\",\"age\":3}")));
+    }
+}
